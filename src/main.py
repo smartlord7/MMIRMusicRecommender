3d856eee40a855695e96aba99bd4436dcf795c21
Sancho Amaral Simões <ssimoes@ipn.pt>
2022-04-07 10:36:25 +0100

    MASTER: Restructure directories and save features to .csv.

--- src/main.py
+++ src/main.py
@@ -1,30 +1,28 @@
 import numpy as np
-
-
-FEATURE_DELIM = ","
-TOP100_FEATURES_PATH = "data/features/top100_features.csv"
+from const import *
 
 
 def min_max_normalize(matrix, a=0, b=1):
     min_val = matrix.min()
     max_val = matrix.max()
-    
+
     matrix = (a + (matrix - min_val) * (b - a)) / (max_val - min_val)
 
     return matrix
 
 
-def process_features(path):
-    matrix = np.genfromtxt(path, delimiter=FEATURE_DELIM)
-    matrix = matrix[1:, 1:matrix.shape[1] - 1]
+def process_features(in_path, out_path):
+    matrix = np.genfromtxt(in_path, delimiter=FEATURE_DELIM)
+    values = matrix[1:, 1:matrix.shape[1] - 1]
 
-    matrix = min_max_normalize(matrix)
+    values = min_max_normalize(values)
+    np.savetxt(out_path, values, delimiter=FEATURE_DELIM)
 
-    return matrix
+    return values
 
 
 def main():
-    print(process_features(TOP100_FEATURES_PATH))
+    process_features(IN_PATH_ORIGINAL_FEATURES, OUT_PATH_ORIGINAL_FEATURES)
 
 
 if __name__ == '__main__':
