0834e6333270224f235340a1536e54bd50a81458
Sancho Amaral Simões <ssimoes@ipn.pt>
2022-05-15 22:53:48 +0100

    MASTER: Improve features.py.

--- src/testing/features.py
+++ src/testing/features.py
@@ -1,20 +1,19 @@
-from os.path import isfile
-
 import numpy as np
+from os.path import isfile
 from const import DELIMITER_FEATURE
 from mmir_pipeline.process import normalize_min_max
 
 
 def process_default_features(in_path, out_path):
     """
-    Function used to process the used features.
-    :param in_path: the input directory.
-    :param out_path: the output directory.
-    :return: required values.
+    Function used to normalize already computed features.
+    :param in_path: the path of the .csv file that contains the features.
+    :param out_path: the path of the file to which the normalized features will be wrriten.
+    :return: the normalized features values.
     """
 
     matrix = np.genfromtxt(in_path, delimiter=DELIMITER_FEATURE)
-    values = matrix[1:, 1:matrix.shape[1] - 1]
+    values = matrix[1:, 1:matrix.shape[1] - 1]  # exclude first column (musics codes) and first row (features names)
     values = normalize_min_max(values)
 
     if isfile(in_path):
